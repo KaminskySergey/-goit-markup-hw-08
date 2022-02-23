db5cea901632896990ef8a7376175e3b20d65b9c
KaminskySergey <kaminsky_s_s@ukr.net>
2022-02-23 23:55:46 +0200

    меню

--- js/menu.js
+++ js/menu.js
@@ -12,3 +12,17 @@
       mobileMenuRef.classList.toggle("is-open");
     });
   })();
+  (() => {
+    const refs = {
+      openMenuBtn: document.querySelector(".menu-open-btn"),
+      closeMenuBtn: document.querySelector(".menu-close-btn"),
+      menu: document.querySelector(".mob-menu"),
+    };
+  
+    refs.openMenuBtn.addEventListener("click", toggleMenu);
+    refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  
+    function toggleMenu() {
+      refs.menu.classList.toggle("is-hidden");
+    }
+  })();
